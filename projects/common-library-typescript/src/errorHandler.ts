2df3421b234aaf193bca0bdf9bc550e9624e5738
Kirill Maksimov <kirill@orbs.network>
2018-02-01 17:36:35 +0200

    Sentry WIP

--- projects/common-library-typescript/src/errorHandler.ts
+++ projects/common-library-typescript/src/errorHandler.ts
@@ -1,8 +1,29 @@
 import * as uncaughtHandler from "uncaught-exception";
 import * as os from "os";
 import { logger } from "./logger";
-
+import { config } from "./config";
+import { Raven } from "raven";
 export type Callback = (callback?: any) => void;
+
+const { NODE_NAME, NODE_IP, NODE_ENV, SERVICE_NAME } = process.env;
+
+const SENTRY_CONFIG_API_KEY = "logger:sentry:apiKey";
+const SETNRY_CONFIG_ENABLED_KEY = "logger:sentry:enabled";
+const SENTRY_ENABLED = config.get(SETNRY_CONFIG_ENABLED_KEY);
+
+const Raven = require('raven');
+
+if (SENTRY_ENABLED) {
+  Raven.config(config.get(SENTRY_CONFIG_API_KEY));
+
+  Raven.context({
+    extra: {
+      node: NODE_NAME,
+      node_ip: NODE_IP,
+      environment: NODE_ENV
+    }
+  })
+}
 
 export class ErrorHandler {
   public static setup(abortOnUncaught: boolean = false, gracefulShutdown?: Callback) {
@@ -11,6 +32,10 @@ export class ErrorHandler {
         fatal: function fatal(message, meta, callback) {
           logger.error(message, meta.error);
           logger.error(meta.error.stack);
+
+          if (SENTRY_ENABLED) {
+            Raven.captureException();
+          }
 
           callback();
         },
