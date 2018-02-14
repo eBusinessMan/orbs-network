02a7580787dcb1d57ed6f64c09ac8bdedc670835
Shai Yallin <shai.yallin@gmail.com>
2018-02-14 13:38:18 +0200

    added the ability to insert the management interface into an existing Mali instance

---
+++ projects/libs/core-library-typescript/src/common-library/grpc-management.ts
@@ -0,0 +1,38 @@
+import { ManagementService } from "./management";
+import * as Mali from "mali";
+import * as caller from "grpc-caller";
+import { types } from "./types";
+import * as path from "path";
+
+const PROTO_PATH = path.resolve(__dirname, "../../../../architecture/interfaces");
+
+class ManagementRPCService {
+  service: ManagementService;
+
+  constructor(service: ManagementService) {
+    this.service = service;
+  }
+
+  async isAlive(ctx: types.IsAliveContext) {
+    ctx.res = {alive: this.service.isAlive()};
+  }
+}
+
+const mgmtName = "Management";
+const mgmtProtoPath = path.resolve(PROTO_PATH, "management.proto");
+
+function addManagementService(app: Mali, management: ManagementService) {
+  const rpcService = new ManagementRPCService(management);
+  app.addService(mgmtProtoPath, mgmtName);
+  const methods: {[key: string]: Function} = {};
+  types.Management.forEach(method => methods[method] = rpcService[method].bind(rpcService));
+  app.use(methods);
+}
+
+function managementClient(endpoint: String) {
+  return caller(endpoint, mgmtProtoPath, mgmtName);
+}
+
+export {
+  addManagementService, managementClient
+};
--- projects/libs/core-library-typescript/test/grpc/grpc.spec.ts
+++ projects/libs/core-library-typescript/test/grpc/grpc.spec.ts
@@ -4,41 +4,69 @@ import * as caller from "grpc-caller";
 import { expect } from "chai";
 import { types } from "../../src/common-library/types";
 import { IsAliveInput, IsAliveContext } from "../../../../architecture/dist/index";
+import { addManagementService, managementClient } from "../../src/common-library/grpc-management";
+import { ManagementService } from "../../src/common-library/management";
 
-import * as pbjs from "protobufjs";
 
-const PROTO_PATH = path.resolve(__dirname, "../../../../architecture/interfaces");
-
-class ManagementService {
-  async isAlive(ctx: IsAliveContext) {
-    ctx.res = {alive: true};
+class DummyManagementService implements ManagementService {
+  isAlive(): boolean {
+    return true;
   }
+}
+
+class GreeterService {
+  async sayHello(ctx: SayHelloContext) {
+    ctx.res = {message: `Hey, ${ctx.req.name}`};
+  }
+}
+
+interface SayHelloContext {
+  req: SayHelloInput;
+  res: SayHelloOutput;
+}
+
+interface GreeterClient {
+  SayHello(SayHelloInput: SayHelloInput): SayHelloOutput;
+}
+
+interface GreeterServer {
+  SayHello(rpc: SayHelloContext): void;
+}
+
+interface SayHelloInput {
+  name: string;
+}
+
+interface SayHelloOutput {
+  message: string;
 }
 
 let app;
-const name = "Management";
-const protoPath = path.resolve(PROTO_PATH, "management.proto");
 const endpoint = "localhost:8080";
-
-function mapMethodsOf<T>(service: T, name: string) {
-  const serviceFuncs: {[key: string]: Function} = {};
-  for (const funcName of (<any>types)[name]) {
-    serviceFuncs[funcName] = (service)[funcName];
-  }
-  return serviceFuncs;
-}
+const greeterName = "Greeter";
+const greeterProtoPath = path.resolve(__dirname, "./greeter.proto");
 
 describe("A gRPC based server", async () => {
   before(async () => {
-    app = new Mali(protoPath, name);
-    const mgmt = new ManagementService();
-    app.use(mapMethodsOf(mgmt, name));
+    app = new Mali(greeterProtoPath, greeterName);
+    const greeter = new GreeterService();
+    app.use({"sayHello": greeter.sayHello});
+
+    addManagementService(app, new DummyManagementService());
+
     app.start(endpoint);
   });
 
-  it("starts a management endpoint", async () => {
+  it("starts the main endpoint", async () => {
 
-    const client = caller(endpoint, protoPath, name);
+    const client = caller(endpoint, greeterProtoPath, greeterName);
+    const res = await client.sayHello({name: "Joe"});
+    return expect(res).to.have.property("message", "Hey, Joe");
+  });
+
+  it("starts the management endpoint", async () => {
+
+    const client = managementClient(endpoint);
     const res = await client.isAlive({});
     return expect(res).to.have.property("alive", true);
   });
